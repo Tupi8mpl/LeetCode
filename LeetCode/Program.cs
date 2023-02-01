8ba0a022c60801345a8f4439e91807c9df9492c4
I12 <i.tupikov12@gmail.com>
2023-02-01 18:54:21 +0500

    Add third task

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var result = s.LengthOfLongestSubstring("aab");
+            Console.WriteLine(result);
+        }
+    }
+}
